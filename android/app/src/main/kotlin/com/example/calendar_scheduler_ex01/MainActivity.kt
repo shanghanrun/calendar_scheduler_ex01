ad6e15af7e7f971040e96558a9f0326068804f35
shanghanrun <idim7@naver.com>
2023-12-28 17:29:08 +0900

    상부달력.

---
+++ android/app/src/main/kotlin/com/example/calendar_scheduler_ex01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calendar_scheduler_ex01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:calendar_scheduler_ex01/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
+}
