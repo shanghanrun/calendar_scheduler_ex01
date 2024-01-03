142a207e6e7c3542eae804c72ef337cbcfa8c992
shanghanrun <idim7@naver.com>
2024-01-03 14:59:25 +0900

    18장완료

--- lib/provider/schedule_provider.dart
+++ lib/provider/schedule_provider.dart
@@ -1,6 +1,7 @@
 import 'package:calendar_scheduler_ex01/model/schedule_model.dart';
 import 'package:calendar_scheduler_ex01/repository/schedule_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class ScheduleProvider extends ChangeNotifier {
   final ScheduleRepository repository;
@@ -31,33 +32,69 @@ class ScheduleProvider extends ChangeNotifier {
     required ScheduleModel schedule,
   }) async {
     final targetDate = schedule.date;
-    final savedSchedule = await repository.createSchedule(schedule: schedule);
+    const uuid = Uuid();
+    final tempId = uuid.v4(); // 유일한 id생성
+    final newSchedule = schedule.copyWith(
+      // cache.update에 넣을 임시 스캐줄
+      id: tempId,
+    );
+    //긍정적 응답구간. 서버에서 응답을 받기 전에 캐시를 먼저 업데이트한다.
     print('cache update 시작');
     cache.update(
       targetDate,
       (value) => [
         ...value,
-        schedule.copyWith(
-          id: savedSchedule,
-        )
+        newSchedule,
       ]..sort((a, b) => a.startTime.compareTo(b.startTime)),
-      ifAbsent: () => [schedule],
+      ifAbsent: () => [newSchedule],
     );
     print('cache 업데이트 다음과 같다.=> $cache');
-    notifyListeners();
+    notifyListeners(); // 캐시 업데이ㅡ 반영하기
+
+    try {
+      // 진짜 데이터를 받아오고, 업데이트 하는 과정, 에러가 발생할 경우에 캐시롤백
+      final savedSchedule = await repository.createSchedule(schedule: schedule);
+      cache.update(
+        targetDate,
+        (value) => value
+            .map((e) => e.id == tempId ? e.copyWith(id: savedSchedule) : e)
+            .toList(),
+      );
+    } catch (e) {
+      // 서버로 저장하는 것 실패하고, 또한 새로운 데이터로 캐시업데이트 실패
+      cache.update(
+        targetDate,
+        (value) => value.where((e) => e.id != tempId).toList(), //filter이다.
+      ); // 캐시를 원래로 되돌림(롤백)
+    }
+    notifyListeners(); // 캐시 변화된 것 화면에 반영됨
   }
 
   void deleteSchedule({
     required DateTime date,
     required String id,
   }) async {
-    final resp = await repository.deleteSchedule(id: id);
+    final targetSchedule = cache[date]!.firstWhere(
+      (e) => e.id == id,
+    ); // 삭제할 일정 기억(schedule) , 나중에 다시 사용할 수 있게
 
     cache.update(
       date,
       (value) => value.where((e) => e.id != id).toList(),
       ifAbsent: () => [],
-    );
+    ); // 삭제전에 캐시를 먼저 업데이트
+    notifyListeners(); // 캐시업데이트 반영하기. 긍정적응답
+
+    try {
+      await repository.deleteSchedule(id: id);
+    } catch (e) {
+      //삭제 실패시, 캐시를 다시 롤백 -> notify 하면 화면에 반영됨
+      cache.update(
+        date,
+        (value) => [...value, targetSchedule] // 삭제한 것과 동일한 스캐줄 다시 넣어줌
+          ..sort((a, b) => a.startTime.compareTo(b.startTime)),
+      );
+    }
     notifyListeners();
   }
 
